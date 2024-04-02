63120086cc11d4cc3fd53c40016f714f9b7511fe
Ji Chen <jaykchen@gmail.com>
2024-04-02 10:20:49 -0400

    client_secret renewed

--- register_hook/query.sh
+++ register_hook/query.sh
@@ -3,11 +3,11 @@ curl -X POST 'https://github.com/login/oauth/access_token' \
      -H 'Content-Type: application/json' \
      -d '{
             "client_id": "816b35c307fe6ebe19c7",
-            "client_secret": "bb626e19d1aa8a0dc91bacb0e4e639691e8826e1",
+            "client_secret": "******",
             "code": "cd098119ab664248ec0c",
             "redirect_uri": "https://code.flows.network/webhook/jKRuADFii4naC7ANMFtL/register"
          }'
 {"error":"bad_verification_code","error_description":"The code passed is incorrect or expired.","error_uri":"https://docs.github.com/apps/managing-oauth-apps/troubleshooting-oauth-app-access-token-request-errors/#bad-verification-code"}% 
 
 
-https://github.com/login/oauth/authorize?client_id=816b35c307fe6ebe19c7&scope=read:user%20user:email&redirect_uri=https://code.flows.network/webhook/jKRuADFii4naC7ANMFtL/register&state=5cf141a504aaf8ad6dbec91485c93d7ba39452651dd70b4e5c083c13cbc9b882
+https://github.com/login/oauth/authorize?client_id=816b35c307fe6ebe19c7&scope=read:user%20user:email&redirect_uri=https://code.flows.network/webhook/jKRuADFii4naC7ANMFtL/register&state=b7c0cb4567ab0181c1126b251ab0083319f38e43f35489dfd851caa0c813e2be
