8cd4e9122771eb9681ef7b978b774fd9cb7b4190
Ji Chen <jaykchen@gmail.com>
2024-04-10 13:58:34 -0400

    data table simplified

--- migrations/20240308202145_gosim.sql
+++ migrations/20240308202145_gosim.sql
@@ -1,17 +1,9 @@
-CREATE TABLE participants (
-    login_id VARCHAR(255) PRIMARY KEY,  -- url of a project repo
-    email  VARCHAR(255),
-    in_event_status ENUM('zero', 'single', 'multiple', 'banned'),
-    his_issues_list JSON
-);
-
 CREATE TABLE projects (
     project_id VARCHAR(255) PRIMARY KEY,  -- url of a project repo
     project_logo VARCHAR(255),
     repo_stars INT,
     project_description TEXT,  -- description of the project, summary of its readme, etc.
     issues_list JSON,
-    issues_flagged JSON,
     participants_list JSON,
     total_budget_allocated INT,
     total_budget_used INT,
@@ -24,10 +16,14 @@ CREATE TABLE issues_master (
     issue_description TEXT NOT NULL,  -- description of the issue, could be truncated body text
     issue_budget INT,
     issue_assignees JSON,    
+    date_issue_assigned DATETIME,   
     issue_linked_pr VARCHAR(255),    -- url of the pull_request that closed the issue, if any, or the pull_request that is linked to the issue
-    issue_status TEXT,    -- default empty, or some situation identified by AI summarizing the issue's comments
+    issue_status TEXT,    -- default empty, or some situation odd conditions occur
     review_status ENUM('queue', 'approve', 'decline'),
+    date_approved DATETIME,
+    date_declined DATETIME,
     issue_budget_approved BOOLEAN
+    date_budget_approved DATETIME,
 );
 
 CREATE TABLE issues_open (
@@ -40,15 +36,15 @@ CREATE TABLE issues_open (
 );
 
 
+CREATE TABLE issues_assigned (
+    issue_id VARCHAR(255) PRIMARY KEY,  -- url of an issue
+    issue_assignees JSON,    
+);
+
 CREATE TABLE issues_closed (
     issue_id VARCHAR(255) PRIMARY KEY,  -- url of an issue
     issue_assignees JSON,    
     issue_linked_pr VARCHAR(255)    -- url of the pull_request that closed the issue, if any, or the pull_request that is linked to the issue
-);
-
-CREATE TABLE issues_comments (
-    issue_id VARCHAR(255) PRIMARY KEY,  -- url of an issue
-    issue_status TEXT    -- default empty, or some situation identified by AI summarizing the issue's comments
 );
 
 
@@ -58,6 +54,5 @@ CREATE TABLE pull_requests (
     pull_author VARCHAR(50) ,
     project_id VARCHAR(255) NOT NULL,
     connected_issues JSON,
-    merged_by VARCHAR(50) ,
-    pull_status TEXT    -- default empty, or some situation exposed by conflicting information
+    date_merged DATETIME,
 );
