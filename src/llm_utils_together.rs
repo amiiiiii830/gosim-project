0a2aeb60562e3d7f16c0e7172e9c168402fd430f
Ji Chen <jaykchen@gmail.com>
2024-04-23 17:06:41 -0400

    log finish reason

--- src/llm_utils_together.rs
+++ src/llm_utils_together.rs
@@ -4,24 +4,25 @@ use reqwest::{
 };
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct ChatResponse {
     pub choices: Vec<ChatChoice>,
     pub model: String,
 }
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct ChatMessage {
     pub content: Option<String>,
     pub role: Role,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct ChatChoice {
+    pub finish_reason: Option<String>,
     pub index: u32,
     pub message: ChatMessage,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Clone, Serialize)]
 pub enum Role {
     #[serde(rename = "system")]
     System,
@@ -41,9 +42,12 @@ pub async fn chat_inner_async(
 ) -> anyhow::Result<String> {
     let mut headers = HeaderMap::new();
     let api_key = std::env::var("TOGETHER_API_KEY")?;
+    let bearer_token = format!("Bearer {}", api_key);
+
     headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
     headers.insert(USER_AGENT, HeaderValue::from_static("MyClient/1.0.0"));
-    headers.insert(AUTHORIZATION, HeaderValue::from_str(&(format!("Bearer {}", api_key)))?);
+    headers.insert(AUTHORIZATION, HeaderValue::from_str(&bearer_token)?);
+    let input_head = input.chars().take(50).collect::<String>();
 
     let messages = serde_json::json!([
         {"role": "system", "content": system_prompt},
@@ -64,12 +68,18 @@ pub async fn chat_inner_async(
     if response.status().is_success() {
         let response_body = response.text().await?;
         if let Ok(chat_response) = serde_json::from_str::<ChatResponse>(&response_body) {
+            let finish_reason = &chat_response.choices[0].clone()
+                .finish_reason
+                .unwrap_or("no finish reason found".to_string());
+            log::info!("input: {}, finish_reason: {}", input_head, finish_reason);
+
             if let Some(content) = &chat_response.choices[0].message.content {
                 return Ok(content.to_string());
             }
         }
         Err(anyhow::anyhow!("error deserialize ChatResposne"))
 
+        // Ok(response_body)
     } else {
         let error_msg = format!(
             "Failed to get a successful response: {:?}",
