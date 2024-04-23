d8da2d69d5b12cf53ca52076cac493bf7e8034bf
Ji Chen <jaykchen@gmail.com>
2024-04-23 16:44:59 -0400

    validated together ai

--- src/llm_utils_together.rs
+++ src/llm_utils_together.rs
@@ -1,161 +1,80 @@
-use reqwest::header::{HeaderMap, HeaderValue, CONTENT_TYPE, USER_AGENT};
-use secrecy::Secret;
-use serde::Deserialize;
-use std::collections::HashMap;
-
-use async_openai::{
-    config::Config,
-    types::{
-        // ChatCompletionFunctionsArgs, ChatCompletionRequestMessage,
-        ChatCompletionRequestSystemMessageArgs,
-        ChatCompletionRequestUserMessageArgs,
-        // ChatCompletionTool, ChatCompletionToolArgs, ChatCompletionToolType,
-        CreateChatCompletionRequestArgs,
-    },
-    Client as OpenAIClient,
+use reqwest::{
+    header::{HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_TYPE, USER_AGENT},
+    ClientBuilder,
 };
+use serde::{Deserialize, Serialize};
 
-
-#[derive(Clone, Debug)]
-pub struct LocalServiceProviderConfig {
-    pub api_base: String,
-    pub headers: HeaderMap,
-    pub api_key: Secret<String>,
-    pub query: HashMap<String, String>,
+#[derive(Debug, Deserialize)]
+pub struct ChatResponse {
+    pub choices: Vec<ChatChoice>,
+    pub model: String,
+}
+#[derive(Debug, Deserialize)]
+pub struct ChatMessage {
+    pub content: Option<String>,
+    pub role: Role,
 }
 
-impl Config for LocalServiceProviderConfig {
-    fn headers(&self) -> HeaderMap {
-        self.headers.clone()
-    }
-
-    fn url(&self, path: &str) -> String {
-        format!("{}{}", self.api_base, path)
-    }
-
-    fn query(&self) -> Vec<(&str, &str)> {
-        self.query
-            .iter()
-            .map(|(k, v)| (k.as_str(), v.as_str()))
-            .collect()
-    }
-
-    fn api_base(&self) -> &str {
-        &self.api_base
-    }
-
-    fn api_key(&self) -> &Secret<String> {
-        &self.api_key
-    }
+#[derive(Debug, Deserialize)]
+pub struct ChatChoice {
+    pub index: u32,
+    pub message: ChatMessage,
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+pub enum Role {
+    #[serde(rename = "system")]
+    System,
+    #[serde(rename = "user")]
+    User,
+    #[serde(rename = "assistant")]
+    Assistant,
+    #[serde(rename = "tool")]
+    Tool,
+    #[serde(rename = "function")]
+    Function,
+}
 pub async fn chat_inner_async(
     system_prompt: &str,
-    user_input: &str,
+    input: &str,
     max_token: u16,
 ) -> anyhow::Result<String> {
     let mut headers = HeaderMap::new();
-    let api_key = std::env::var("TOGETHER_API_KEY").expect("TOGETHER_API_KEY must be set");
+    let api_key = std::env::var("TOGETHER_API_KEY")?;
     headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
     headers.insert(USER_AGENT, HeaderValue::from_static("MyClient/1.0.0"));
-    let config = LocalServiceProviderConfig {
-        api_base: String::from("https://api.together.xyz/v1/chat/completions"),
-        headers: headers,
-        api_key: Secret::new(api_key),
-        query: HashMap::new(),
-    };
+    headers.insert(AUTHORIZATION, HeaderValue::from_str(&(format!("Bearer {}", api_key)))?);
 
-    // stop: ['</s>', '[/INST]'],
-    let model = "meta-llama/Llama-3-8b-chat-hf";
-    let client = OpenAIClient::with_config(config);
-    let messages = vec![
-        ChatCompletionRequestSystemMessageArgs::default()
-            .content(system_prompt)
-            .build()
-            .expect("Failed to build system message")
-            .into(),
-        ChatCompletionRequestUserMessageArgs::default()
-            .content(user_input)
-            .build()?
-            .into(),
-    ];
-    let request = CreateChatCompletionRequestArgs::default()
-        .max_tokens(max_token)
-        .model(model)
-        .messages(messages)
-        .build()?;
+    let messages = serde_json::json!([
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": input}
+    ]);
 
-    let chat = match client.chat().create(request).await {
-        Ok(chat) => {
+    let uri = "https://api.together.xyz/v1/chat/completions";
+    let body = serde_json::to_vec(&serde_json::json!({
+        "temperature": 0.7,
+        "max_tokens": max_token,
+        "model": "meta-llama/Llama-3-8b-chat-hf",
+        "messages": messages,
+    }))?;
 
-            chat},
-        Err(_e) => {
-            log::info!("Error getting response from OpenAI: {:?}", _e);
-            return Err(anyhow::anyhow!("Failed to get reply from OpenAI: {:?}", _e));
+    let client = ClientBuilder::new().default_headers(headers).build()?;
+    let response = client.post(uri).body(body).send().await?;
+
+    if response.status().is_success() {
+        let response_body = response.text().await?;
+        if let Ok(chat_response) = serde_json::from_str::<ChatResponse>(&response_body) {
+            if let Some(content) = &chat_response.choices[0].message.content {
+                return Ok(content.to_string());
+            }
         }
-    };
-            let raw = chat.choices.to_owned();
-            log::info!("Error getting response from OpenAI: {:?}", raw);
+        Err(anyhow::anyhow!("error deserialize ChatResposne"))
 
-    match chat.choices[0].message.clone().content {
-        Some(res) => {
-            // log::info!("{:?}", chat.choices[0].message.clone());
-            Ok(res)
-        }
-        None => Err(anyhow::anyhow!("Failed to get reply from OpenAI")),
+    } else {
+        let error_msg = format!(
+            "Failed to get a successful response: {:?}",
+            response.status()
+        );
+        Err(anyhow::anyhow!(error_msg))
     }
 }
-
-pub fn parse_summary_from_raw_json(input: &str) -> String {
-    #[derive(Deserialize, Debug)]
-    struct SummaryStruct {
-        impactful: Option<String>,
-        alignment: Option<String>,
-        patterns: Option<String>,
-        synergy: Option<String>,
-        significance: Option<String>,
-    }
-
-    let summary: SummaryStruct = serde_json::from_str(input).expect("Failed to parse summary JSON");
-
-    let fields = [
-        &summary.impactful,
-        &summary.alignment,
-        &summary.patterns,
-        &summary.synergy,
-        &summary.significance,
-    ];
-
-    fields
-        .iter()
-        .filter_map(|&field| field.as_ref()) // Convert Option<&String> to Option<&str>
-        .filter(|field| !field.is_empty()) // Filter out empty strings
-        .fold(String::new(), |mut acc, field| {
-            if !acc.is_empty() {
-                acc.push_str(" ");
-            }
-            acc.push_str(field);
-            acc
-        })
-}
-
-
-
-pub fn parse_issue_summary_from_json(input: &str) -> anyhow::Result<Vec<(String, String)>> {
-    let parsed: serde_json::Map<String, serde_json::Value> = serde_json::from_str(input)?;
-
-    let summaries = parsed
-        .iter()
-        .filter_map(|(key, value)| {
-            if let Some(summary_str) = value.as_str() {
-                Some((key.clone(), summary_str.to_owned()))
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<(String, String)>>(); // Collect into a Vec of tuples
-
-    Ok(summaries)
-}
-
-
