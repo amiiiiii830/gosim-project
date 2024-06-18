ef33ae8964ff78ced9c124e45c40c0bf5e269e24
Ji Chen <jaykchen@gmail.com>
2024-06-18 08:53:40 -0400

    track live

--- track_github/src/lib.rs
+++ track_github/src/lib.rs
@@ -11,7 +11,7 @@ pub async fn on_deploy() {
     let now = Utc::now();
     let now_minute = now.minute() + 2;
     let cron_time = format!("{:02} {:02} * * *", now_minute, now.hour());
-    let cron_time = String::from("39 * * * *");
+    let cron_time = String::from("57 * * * *");
     schedule_cron_job(cron_time, String::from("cron_job_evoked")).await;
 }
 
@@ -22,35 +22,10 @@ async fn handler(body: Vec<u8>) {
 }
 
 pub async fn inner(_body: Vec<u8>) -> anyhow::Result<()> {
-    // let query = "repo:SarthakKeshari/calc_for_everything is:pr is:merged label:hacktoberfest-accepted created:2023-10-01..2023-10-03 review:approved -label:spam -label:invalid";
-    // let query = "label:hacktoberfest is:issue is:open no:assignee created:2023-10-01..2023-10-03 -label:spam -label:invalid";
-
-    // db_updater::test_add_project().await;
-    // db_updater::test_project_exists().await;
-
-    // let issues = search_issues_open(&query).await?;
-    // let query = "repo:SarthakKeshari/calc_for_everything is:pr is:merged label:hacktoberfest-accepted created:2023-10-01..2023-10-30 review:approved -label:spam -label:invalid";
-    // let pulls = get_per_repo_pull_requests(&query).await?;
-
-    // let mut count = 0;
-    // for iss in pulls {
-    //     count += 1;
-    //     log::error!("pull: {:?}", iss);
-    //     let content = format!("{:?}", iss);
-    //     // let _ = upload_to_gist(&content).await?;
-    //     if count > 5 {
-    //         break;
-    //     }
-    // }
 
     logger::init();
     let pool = get_pool().await;
     let _ = run_hourly(&pool).await;
 
-    Ok(())
-}
-
-pub async fn search_pulls() -> anyhow::Result<()> {
-    // let _ = upload_to_gist(&texts).await?;
     Ok(())
 }
